98d78fc711088a1db347733d6d86051edc682065
Markussim <markus.simonsen@gmail.com>
2021-04-18 15:58:50 +0200

    Added sql

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get("/", async (req, res) => {
 });
 
 app.get("/get", async (req, res) => {
-  let text = await (await pool.query("SELECT * FROM Users")).rows;
+  let text = await (await pool.query("FULL OUTER JOIN ")).rows;
   res.send(text);
 });
 
@@ -37,6 +37,35 @@ app.post("/addUser", async (req, res) => {
     );
     await pool.query(
       "CREATE TABLE " + req.body.UserName + " (Post varchar(255));"
+    );
+  }
+
+  res.redirect("/get");
+});
+
+app.get("/post", async (req, res) => {
+  res.sendFile(__dirname + "/client/post.html");
+});
+
+app.post("/post", async (req, res) => {
+  console.log((await (
+    await pool.query(
+      "SELECT table_name FROM information_schema.tables WHERE table_name = $1;",
+      [req.body.UserName]
+    )
+  ).rows))
+  if (
+    (await (
+      await pool.query(
+        "SELECT table_name FROM information_schema.tables WHERE table_name = $1;",
+        [req.body.UserName]
+      )
+    ).rows.length) == 1
+  ) {
+    console.log("Added")
+    await pool.query(
+      "INSERT INTO " + req.body.UserName + "(Post) VALUES ($1);",
+      [req.body.post]
     );
   }
 
